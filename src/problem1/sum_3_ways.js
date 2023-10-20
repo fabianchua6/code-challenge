2659300b66855aecbc009edb52eee3a06e6326bd
Fabian Chua <fabianchua6@gmail.com>
2023-10-21 00:19:39 +0800

    add sum 3 ways functions

---
+++ src/problem1/sum_3_ways.js
@@ -0,0 +1,27 @@
+// recursively
+var sum_to_n_a = function (n) {
+	let result = 0
+	if (n < 0) {
+		return 0
+	}
+
+	for (let i = 0; i <= n; i++) {
+		result += i
+	}
+	return result
+}
+
+// iteratively
+var sum_to_n_b = function (n) {
+	// base case
+	if (n < 0) {
+		return 0
+	}
+	// recursive case
+	return n + sum_to_n_b(n - 1)
+}
+
+// primary school formula n(n+1)/2
+var sum_to_n_c = function (n) {
+	return (n * (n + 1)) / 2
+}
