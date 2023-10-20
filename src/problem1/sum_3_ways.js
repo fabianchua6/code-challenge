59b39a10676db4c83fe347862952f8ee6c7a6b06
Fabian Chua <fabianchua6@gmail.com>
2023-10-21 00:41:25 +0800

    refactor sum_3_way_c to cater negative numbers

--- src/problem1/sum_3_ways.js
+++ src/problem1/sum_3_ways.js
@@ -23,5 +23,8 @@ var sum_to_n_b = function (n) {
 
 // primary school formula n(n+1)/2
 var sum_to_n_c = function (n) {
-	return (n * (n + 1)) / 2
+	return n < 0 ? 0 : (n * (n + 1)) / 2
 }
+
+// for test
+module.exports = { sum_to_n_a, sum_to_n_b, sum_to_n_c }
