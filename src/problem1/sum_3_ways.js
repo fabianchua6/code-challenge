f406c1901076930e4c97c15aa3ac075db7fe2499
Fabian Chua <fabianchua6@gmail.com>
2023-10-23 23:02:20 +0800

    refactor sum3ways as per lint

--- src/problem1/sum_3_ways.js
+++ src/problem1/sum_3_ways.js
@@ -1,30 +1,30 @@
 // recursively
 var sum_to_n_a = function (n) {
-	let result = 0
-	if (n < 0) {
-		return 0
-	}
+  let result = 0;
+  if (n < 0) {
+    return 0;
+  }
 
-	for (let i = 0; i <= n; i++) {
-		result += i
-	}
-	return result
-}
+  for (let i = 0; i <= n; i++) {
+    result += i;
+  }
+  return result;
+};
 
 // iteratively
 var sum_to_n_b = function (n) {
-	// base case
-	if (n < 0) {
-		return 0
-	}
-	// recursive case
-	return n + sum_to_n_b(n - 1)
-}
+  // base case
+  if (n < 0) {
+    return 0;
+  }
+  // recursive case
+  return n + sum_to_n_b(n - 1);
+};
 
 // primary school formula n(n+1)/2
 var sum_to_n_c = function (n) {
-	return n < 0 ? 0 : (n * (n + 1)) / 2
-}
+  return n < 0 ? 0 : (n * (n + 1)) / 2;
+};
 
 // for test
-module.exports = { sum_to_n_a, sum_to_n_b, sum_to_n_c }
+module.exports = { sum_to_n_a, sum_to_n_b, sum_to_n_c };
